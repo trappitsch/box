4867d1ed0b8156f842d778cb06cc024a785230f1
Reto Trappitsch <reto@galactic-forensics.space>
2024-02-20 15:53:11 +0100

    remove my temporary environmental variation

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,3 @@
-import os
-
 # fixme: pycharm does not recognize the `rye` command, fix path for linux
-if not os.environ["PATH"].__contains__("rye"):
-    os.environ["PATH"] += f":{os.path.expanduser('~')}/.rye/shims"
+# if not os.environ["PATH"].__contains__("rye"):
+#     os.environ["PATH"] += f":{os.path.expanduser('~')}/.rye/shims"
