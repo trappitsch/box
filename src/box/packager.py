5ccabbdaaf032f35411b251af049b25e48360de6
Reto Trappitsch <reto@galactic-forensics.space>
2024-02-21 08:51:23 +0100

    format and lint

--- src/box/packager.py
+++ src/box/packager.py
@@ -15,7 +15,7 @@ import box.utils as ut
 
 PYAPP_SOURCE = "https://github.com/ofek/pyapp/releases/latest/download/source.tar.gz"
 
-lgit
+
 class PackageApp:
     """Package the project with PyApp."""
 
